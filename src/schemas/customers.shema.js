096c897eab1ae259ad6d70f158c1f7e41c515758
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-02-10 19:07:32 -0300

    fix: changing birthday schema to .date()

--- src/schemas/customers.shema.js
+++ src/schemas/customers.shema.js
@@ -4,6 +4,6 @@ export const customerSchema = joi.object({
   name: joi.string().min(1).required(),
   phone: joi.string().min(1).max(11).required(),
   cpf: joi.string().min(1).max(12).required(),
-  birthday: joi.string().min(1).max(10).required(),
+  birthday: joi.date().required(),
 });
 
