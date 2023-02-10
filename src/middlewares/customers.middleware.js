6ae20bdff50cc5e7dd2d869a89af81406c799e39
luccaviccini <luccaviccini@gmail.com>
2023-02-10 19:11:04 -0300

    feat: customers middleware

---
+++ src/middlewares/customers.middleware.js
@@ -0,0 +1,23 @@
+import { db } from "../database/database.connection.js";
+import { customerSchema } from "../schemas/customers.shema.js";
+
+export async function validateCustomer(req, res, next) {
+  // req.body is the customer object
+
+  const { error } = customerSchema.validate(req.body, { abortEarly: false });
+  
+  if (error) {
+    const errors = error.details.map((err) => err.message);
+    return res.status(400).send(errors);
+  }
+
+  const cpfExists = await db.query("SELECT * FROM customers WHERE cpf = $1", [
+    req.body.cpf,
+  ]);
+  if (cpfExists.rowCount > 0) {
+    return res.status(409).send("Customer already exists");
+  }
+
+  res.locals.customer = req.body;
+  next();
+}
