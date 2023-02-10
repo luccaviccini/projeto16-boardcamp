f5cb20263d0144b6a2245ff6c2eda4ffdf370f74
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-02-10 19:37:25 -0300

    fix: cpf validating using length(11) method from joi library

--- src/schemas/customers.shema.js
+++ src/schemas/customers.shema.js
@@ -3,7 +3,7 @@ import joi from 'joi';
 export const customerSchema = joi.object({
   name: joi.string().min(1).required(),
   phone: joi.string().min(1).max(11).required(),
-  cpf: joi.string().min(1).max(12).required(),
+  cpf: joi.string().length(11).required(),
   birthday: joi.date().required(),
 });
 
