7d232eb39da9013362a53f47c444123bb8169f93
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-02-10 20:42:23 -0300

    fix: changing birthday validation to regex

--- src/schemas/customers.shema.js
+++ src/schemas/customers.shema.js
@@ -4,6 +4,6 @@ export const customerSchema = joi.object({
   name: joi.string().min(1).required(),
   phone: joi.string().min(10).max(11).required(),
   cpf: joi.string().length(11).pattern(/^[0-9]+$/).required(),
-  birthday: joi.date().required(),
+  birthday: joi.string().pattern(/^\d{4}-\d{2}-\d{2}$/).required(),
 });
 
