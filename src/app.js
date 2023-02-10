cfd9ab797e579a041ef8910833a749eef32a2139
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-02-10 19:36:25 -0300

    feat: importing and using customers Router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import gamesRouter from "./routers/games.router.js";
+import customersRouter from "./routers/customers.router.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(gamesRouter);
+app.use(customersRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
