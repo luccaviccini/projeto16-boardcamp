08b5ae7963de33006f642604c50df62e7cb41026
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-02-10 20:41:06 -0300

    feat: if method is put do not check if cpf exists

--- src/middlewares/customers.middleware.js
+++ src/middlewares/customers.middleware.js
@@ -5,10 +5,16 @@ export async function validateCustomer(req, res, next) {
   // req.body is the customer object
 
   const { error } = customerSchema.validate(req.body, { abortEarly: false });
-  
+
   if (error) {
     const errors = error.details.map((err) => err.message);
     return res.status(400).send(errors);
+  }
+
+  // if merhod is PUT, check if customer exists
+  if (req.method === "PUT") {
+    res.locals.customer = req.body;
+    return next();
   }
 
   const cpfExists = await db.query("SELECT * FROM customers WHERE cpf = $1", [
