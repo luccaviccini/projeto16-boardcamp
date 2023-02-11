76d6b55c1b14fac51e27bca6c2bfd0fa4b5f6afe
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-02-11 17:22:57 -0300

    feat: readRentals

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -2,8 +2,8 @@ import { db } from "../database/database.connection.js";
 
 export async function createRental(req, res) {
   const { customerId, gameId, daysRented } = res.locals.rental;
-  const  pricePerDay  = res.locals.pricePerDay;
-  console.log("TO CHEGANDO AQUI: ", pricePerDay)
+  const pricePerDay = res.locals.pricePerDay;
+  console.log("TO CHEGANDO AQUI: ", pricePerDay);
 
   const rentalObject = {
     customerId,
@@ -30,38 +30,67 @@ export async function createRental(req, res) {
     );
     res.sendStatus(201);
   } catch (error) {
-    
     res.status(500).send(error.message);
   }
 }
 
+// get All rentals
+
+export async function readRentals(req, res) {
+  const { customerId, gameId } = req.query;
+
+  // global query that ele
+  let query = `SELECT json_build_object(
+    'id', rentals.id,
+    'customerId', rentals."customerId",
+    'gameId', rentals."gameId",
+    'rentDate', rentals."rentDate",
+    'daysRented', rentals."daysRented",
+    'returnDate', rentals."returnDate",
+    'originalPrice', rentals."originalPrice",
+    'delayFee', rentals."delayFee",
+    'customer', json_build_object(
+        'id', customers.id,
+        'name', customers.name
+    ),
+    'game', json_build_object(
+        'id', games.id,
+        'name', games.name
+    )
+  ) AS result
+    FROM rentals
+    JOIN customers ON rentals."customerId" = customers.id
+    JOIN games ON rentals."gameId" = games.id;`;
+
+  try {
+    const rentals = await db.query(query);
+    res.send(rentals.rows);
+  } catch (error) {
+    res.status(500).send("Error getting rentals");
+  }
+}
 
 // delete rental
 
 export async function deleteRental(req, res) {
   const { id } = req.params;
-  
+  console.log("ID: ", id);
+
   try {
-    const result = await db.query(
-      `SELECT * FROM rentals WHERE id = $1`,
-      [id]
-    );
+    const result = await db.query(`SELECT * FROM rentals WHERE id = $1`, [id]);
     //VERIFICANDO SE O ID  EXISTE
     if (result.rowCount === 0) {
       return res.sendStatus(404);
     }
     //VERIFICANDO SE O ALUGUEL JÁ FOI FINALIZADO
-    if(!result.rows[0].returnDate ){
+    console.log("RESULT: ", result.rows[0]);
+    if (!result.rows[0].returnDate) {
       return res.sendStatus(400);
     }
 
-    await db.query(
-      `DELETE FROM rentals WHERE id = $1`,
-      [id]
-    );
+    await db.query(`DELETE FROM rentals WHERE id = $1`, [id]);
     res.sendStatus(200);
   } catch (error) {
     res.status(500).send("Error deleting rental");
   }
-
 }
--- src/routers/rentals.router.js
+++ src/routers/rentals.router.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 
-import {createRental, deleteRental} from "../controllers/rentals.controller.js";
+import {
+  createRental,
+  readRentals,
+  deleteRental,
+} from "../controllers/rentals.controller.js";
 import {validateRental} from "../middlewares/rentals.middleware.js";
 
 
 const router = Router();
 
 router.post("/rentals", validateRental, createRental);
+router.get("/rentals", readRentals);
 router.delete("/rentals/:id", deleteRental);
 
 export default router;
