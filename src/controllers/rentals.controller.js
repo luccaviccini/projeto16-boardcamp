1b9f502aec4f1473fdb3ad998c319f697fcd011f
luccaviccini <luccaviccini@gmail.com>
2023-02-11 16:03:18 -0300

    feat: delete Rental

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -34,3 +34,34 @@ export async function createRental(req, res) {
     res.status(500).send(error.message);
   }
 }
+
+
+// delete rental
+
+export async function deleteRental(req, res) {
+  const { id } = req.params;
+  
+  try {
+    const result = await db.query(
+      `SELECT * FROM rentals WHERE id = $1`,
+      [id]
+    );
+    //VERIFICANDO SE O ID  EXISTE
+    if (result.rowCount === 0) {
+      return res.sendStatus(404);
+    }
+    //VERIFICANDO SE O ALUGUEL JÁ FOI FINALIZADO
+    if(!result.rows[0].returnDate ){
+      return res.sendStatus(400);
+    }
+
+    await db.query(
+      `DELETE FROM rentals WHERE id = $1`,
+      [id]
+    );
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).send("Error deleting rental");
+  }
+
+}
--- src/routers/rentals.router.js
+++ src/routers/rentals.router.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
-import {createRental} from "../controllers/rentals.controller.js";
+import {createRental, deleteRental} from "../controllers/rentals.controller.js";
 import {validateRental} from "../middlewares/rentals.middleware.js";
+
 
 const router = Router();
 
 router.post("/rentals", validateRental, createRental);
+router.delete("/rentals/:id", deleteRental);
 
 export default router;
 
